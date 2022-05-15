59120a4a0b8cc60737f7e7a825b539f749bc939e
Peter Feng <37753741+Coypirus@users.noreply.github.com>
2022-05-14 22:20:35 -0400

    Add Audio Recording Functionality as a Foreground Service with Saving as a WAV file and a space for working with sampled audio.

--- app/src/main/java/com/example/dolphinchat/AudioRecorderService.java
+++ app/src/main/java/com/example/dolphinchat/AudioRecorderService.java
@@ -1,21 +1,76 @@
 package com.example.dolphinchat;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
 import android.app.Notification;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
+import android.media.AudioFormat;
+import android.media.AudioRecord;
 import android.media.MediaRecorder;
+import android.os.Build;
 import android.os.IBinder;
+import android.util.Log;
+import android.widget.Toast;
 
-import androidx.core.app.NotificationCompat;
+import androidx.annotation.RequiresApi;
+
 
 public class AudioRecorderService extends Service {
-    private MediaRecorder recorder;
+    private static boolean isRunning = false;
+    private static final int AUDIO_SOURCE = MediaRecorder.AudioSource.MIC;
+    private static final int SAMPLE_RATE = 44100; //CHANGE THIS AS NEEDED.
+    private static final int CHANNEL_CONFIG = AudioFormat.CHANNEL_IN_MONO;
+    private static final int AUDIO_FORMAT = AudioFormat.ENCODING_PCM_16BIT;
+    private static final int BUFFER_SIZE_RECORDING = AudioRecord.getMinBufferSize(SAMPLE_RATE, CHANNEL_CONFIG, AUDIO_FORMAT);
+
+
+    private String fileName;
+    private AudioRecord audioRecord;
+    private AudioRecordThread audioRecordThread;
+
+    private final class AudioRecordThread extends Thread{
+        @SuppressLint("MissingPermission")
+        @Override
+        public void run() {
+            try {
+                audioRecord = new AudioRecord(AUDIO_SOURCE, SAMPLE_RATE, CHANNEL_CONFIG, AUDIO_FORMAT, BUFFER_SIZE_RECORDING);
+            } catch (Exception e){
+                Log.e("AudioRecordThread", "error initializing ");
+                e.printStackTrace();
+            }
+            if (audioRecord.getState() != AudioRecord.STATE_INITIALIZED) { // check for proper initialization
+                Log.e("AudioRecordThread", "error initializing ");
+                return;
+            }
+
+            audioRecord.startRecording();
+            Log.i("AudioRecordThread", "Starting loop");
+            //Get recording data and perform all necessary tasks on it.
+            short[] data = new short[BUFFER_SIZE_RECORDING/2];
+            WavWriter wavWriter = new WavWriter(SAMPLE_RATE);
+            wavWriter.start();
+            Log.w("WavWriter", "Starting writing to path: " + wavWriter.getPath());
+
+            while(isRunning) {
+                //Read audio data
+                int numShortsRead = audioRecord.read(data, 0, data.length);
+                //Write audio data to WAV file.
+                wavWriter.pushAudioShort(data, numShortsRead);
+                Log.i("AudioRecordThread", "Wrote " + numShortsRead + " shorts to wav file.");
+                //Do processing with tensorflow lite here?
+            }
+            Log.w("WavWriter" , "Seconds written: " + wavWriter.secondsWritten() + " ");
+            wavWriter.stop();
+            audioRecord.stop();
+
+        }
+    }
 
 
 
     public AudioRecorderService() {
-        //Need to request permission to record audio.
-
 
     }
 
@@ -25,13 +80,48 @@ public class AudioRecorderService extends Service {
         throw new UnsupportedOperationException("Not yet implemented");
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     public int onStartCommand (Intent intent, int flags, int startId) {
-        String fileName = intent.getStringExtra("fileName");
+        isRunning = true;
+        Toast.makeText(this, "audio recording service started", Toast.LENGTH_SHORT).show();
+
+        //Send notification for service start.
+
+        Intent notificationIntent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent =
+                PendingIntent.getActivity(this, 0, notificationIntent, 0);
+
+        Notification notification =
+                new Notification.Builder(this, "CHANNEL_DEFAULT_IMPORTANCE")
+                        .setContentTitle("Recording Service")
+                        .setContentText("Recording Service Running")
+                        .setSmallIcon(R.drawable.ic_launcher_foreground)
+                        .setContentIntent(pendingIntent)
+                        .setTicker("Recording Service Running")
+                        .build();
+
+        // Notification ID cannot be 0.
+        startForeground(2, notification);
+
+
+        fileName = intent.getStringExtra("fileName");
+        audioRecordThread = new AudioRecordThread();
+        audioRecordThread.start();
         return START_STICKY;
 
         //Set up recorder.
 
     }
+
+    @Override
+    public void onDestroy() {
+        isRunning = false;
+        Toast.makeText(this, "audio recording service done", Toast.LENGTH_SHORT).show();
+    }
+    public static boolean isRunning() {
+        return isRunning;
+    }
+
 
 }
--- app/src/main/java/com/example/dolphinchat/MainActivity.java
+++ app/src/main/java/com/example/dolphinchat/MainActivity.java
@@ -1,36 +1,87 @@
 package com.example.dolphinchat;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
 
 
     private static final String CHANNEL_DEFAULT_ID = "CHANNEL_DEFAULT_IMPORTANCE";
+    private static final int REQUEST_RECORD_AUDIO_EXTERNAL_STORAGE_PERMISSION = 200;
+
+    // Requesting permission to RECORD_AUDIO and  WRITE_EXTERNAL_STORAGE
+    private boolean permissionToRecordAccepted = false;
+    private boolean permissionToWriteExternalStorageAccepted = false;
+    private boolean permissionToReadExternalStorageAccepted = false;
+    private String [] permissions = {Manifest.permission.RECORD_AUDIO, Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE};
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode){
+            case REQUEST_RECORD_AUDIO_EXTERNAL_STORAGE_PERMISSION:
+                permissionToRecordAccepted  = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                permissionToWriteExternalStorageAccepted  = grantResults[1] == PackageManager.PERMISSION_GRANTED;
+                permissionToReadExternalStorageAccepted  = grantResults[2] == PackageManager.PERMISSION_GRANTED;
+                break;
+        }
+        if (!permissionToRecordAccepted || !permissionToWriteExternalStorageAccepted || !permissionToReadExternalStorageAccepted) {
+            Log.w("Permission to Record: ", String.valueOf(permissionToRecordAccepted));
+            Log.w("Permission to Write: ", String.valueOf(permissionToWriteExternalStorageAccepted));
+            Log.w("Permission to Read: ", String.valueOf(permissionToReadExternalStorageAccepted));
+            finish();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         //Create a notification channel so we can send notifications(Required for Foreground Service)
         createNotificationChannelDefault();
+        //Request permission to record audio and write external storage.
+        ActivityCompat.requestPermissions(this, permissions, REQUEST_RECORD_AUDIO_EXTERNAL_STORAGE_PERMISSION);
+
+
         //Set up hello service button to start hello foreground service.
-        Button startHelloServiceButton = (Button) findViewById(R.id.startHelloServiceButton);
+        Button startHelloServiceButton = (Button) findViewById(R.id.btn_hello_service);
         startHelloServiceButton.setOnClickListener(new View.OnClickListener() {
             @RequiresApi(api = Build.VERSION_CODES.O)
             @Override
             public void onClick(View v) {
                 startHelloService();
+            }
+        });
+        Button startAudioRecordingButton = (Button) findViewById(R.id.btn_start_recording);
+        startAudioRecordingButton.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.O)
+            @Override
+            public void onClick(View v) {
+                startAudioRecordingService("my_file");
+            }
+        });
+        Button stopAudioRecordingButton = (Button) findViewById(R.id.btn_stop_recording);
+        stopAudioRecordingButton.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.O)
+            @Override
+            public void onClick(View v) {
+                stopAudioRecordingService();
             }
         });
 
@@ -60,10 +111,30 @@ public class MainActivity extends AppCompatActivity {
         context.startForegroundService(intent);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     private void startAudioRecordingService(String fileName) {
         Context context = getApplicationContext();
         Intent intent = new Intent(this, AudioRecorderService.class);
         intent.putExtra("fileName", fileName);
-        context.startForegroundService(intent);
+        if(!AudioRecorderService.isRunning()) {
+            context.startForegroundService(intent);
+        }
     }
+    private void stopAudioRecordingService() {
+        Context context = getApplicationContext();
+        Intent intent = new Intent(this, AudioRecorderService.class);
+        if(AudioRecorderService.isRunning()) {
+            context.stopService(intent);
+        } else {
+            /*
+            For Testing Purposes only.
+
+            File path = new File(Environment.getExternalStorageDirectory().getPath() + "/Record");
+            if (!path.exists() && !path.mkdirs()) {
+                Log.e("MainActivity", "Failed to make directory: " + path.toString());
+            }
+            */
+        }
+    }
+
 }
